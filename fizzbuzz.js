aa9e6b799c1aa671981f018e00c5994d76d6be1e
Alexis <53586057+alexiscameronfrankel@users.noreply.github.com>
2020-02-14 21:44:08 -0500

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 50; i++) {
+   if (i % 10 === 0 || i % 15 === 0)  {
+    console.log("Donkey");
+  }  else if  (i % 2 != 0 && (i-1) % 10 === 0) {
+      console.log("Monkey");
+    }
+  else if (i % 5 === 0){
+    continue;
+  } else {
+    console.log(i);
+  }
+}
+
